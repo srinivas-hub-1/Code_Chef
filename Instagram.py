fc367ac657355db54cf04ac47e523ef15a9720d4
PRSD <prsdsrinivas7ai@gmail.com>
2025-07-28 15:57:30 +0530

    Instagram.py

---
+++ Instagram.py
@@ -0,0 +1,12 @@
+T=int(input())
+ 
+for _ in range(T):
+    x,y=map(int , input().split())
+    
+    if x > y*10:
+        
+        print(" Yes ")
+        
+    else:
+        
+        print(" No ")
